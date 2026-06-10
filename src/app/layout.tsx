6a7ef905f4e2c44ad0c65dfdb8ec4d0926cd439d
namngxthanh <namngx163@gmail.com>
2026-06-10 08:57:47 +0700

    feat: integrate nextjs-toploader and update prisma logging configuration based on environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,6 +29,17 @@ export default function RootLayout({
     <html lang="vi" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased overflow-x-hidden`}>
         <SessionProvider>
+          <NextTopLoader
+            color="#f59e0b"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #f59e0b,0 0 5px #f59e0b"
+          />
           {children}
           <Toaster position="top-right" richColors />
         </SessionProvider>
